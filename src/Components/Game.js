17b04d5ef3890d8a1d07aad2610beea64b81fea4
Diana0806 <diananovak871@gmail.com>
2023-11-20 15:59:16 +0100

    add Partie 5

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -2,15 +2,31 @@ import React from 'react';
 import './Game.css';
 import Monster from './Monster';
 import PlayerList from './PlayerList';
+import { useSelector } from 'react-redux';
+import GameOver from './GameOver';
+import Victory from './Victory';
 
-const App = () => (
-  <div className="App">
+const App = () => {
+  const status = useSelector((state) => state.fight.status);
+  
+  return (
+    <div className="App">
+    {status === "ONGOING" ? (
+    <>
     <Monster />
     <br></br>
     <section className="container-fluid">
       <PlayerList />
     </section>
+    </>
+
+    ) : status === "LOSE" ? (
+      <GameOver />
+    ) : (
+      <Victory />
+    )}
   </div>
-)
+  );
+};
 
 export default App;
